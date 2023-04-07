7f4db7e9fed66e0067b0fb3d5fed9402bfc250f3
Aqil Mahmud <akilmahmood98@gmail.com>
2023-04-07 23:18:15 +0600

    Final Commit

---
+++ src/product/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Variant, Product, ProductImage, ProductVariant, ProductVariantPrice
+
+admin.site.register(Variant)
+admin.site.register(Product)
+admin.site.register(ProductImage)
+admin.site.register(ProductVariant)
+admin.site.register(ProductVariantPrice)
---
+++ src/product/views/product.py
@@ -0,0 +1,45 @@
+from django.views import generic
+from django.views.generic import ListView, CreateView, UpdateView
+
+from product.forms import ProductForm
+from product.models import Variant, Product
+
+
+class CreateProductView(generic.TemplateView):
+    template_name = 'products/create.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateProductView, self).get_context_data(**kwargs)
+        variants = Variant.objects.filter(active=True).values('id', 'title')
+        context['product'] = True
+        context['variants'] = list(variants.all())
+        return context
+    
+
+class BaseProductView(generic.View):
+    form_class = ProductForm
+    model = Product
+    template_name = 'product/create.html'
+    success_url = '/product/product'
+
+    
+
+class ProductView(BaseProductView, ListView):
+    template_name = 'products/list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        filter_string = {}
+        print(self.request.GET)
+        for key in self.request.GET:
+            if self.request.GET.get(key):
+                filter_string[key] = self.request.GET.get(key)
+        return Product.objects.filter(**filter_string)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
+        context['request'] = ''
+        if self.request.GET:
+            context['request'] = self.request.GET['title__icontains']
+        return context
